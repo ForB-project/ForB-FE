5f72e75510e8508fa41ac2f973b9e083184ecf34
Dotoli <zzphoodev@gmail.com>
2022-09-20 19:50:42 +0900

    Bug: fix index line

--- src/static/index.js
+++ src/static/index.js
@@ -4,8 +4,6 @@ import FEvsBE from "./frontback.png";
 import mainFirst from "./madeby.png";
 import main2 from "./madeby2.png";
 import main3 from "./madeby3.png";
-export { main3, main2, mainFirst, MainBackImg, FEvsBE };
 
-import GreateHall from "./PixelGreateHall.png"
-export { Dog, MainBackImg, FEvsBE,GreateHall,main2,main3,mainFirst  };
-
+import GreateHall from "./PixelGreateHall.png";
+export { MainBackImg, FEvsBE, GreateHall, main2, main3, mainFirst };
