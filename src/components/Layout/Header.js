20ac0deb420cda5aaeb7100ad3f8a2e261f29bd5
Dotoli <zzphoodev@gmail.com>
2022-09-20 19:04:23 +0900

    Design: layout color change

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,6 +9,9 @@ export default Header;
 
 const HeaderSyled = styled.div`
   /* border: 1px dashed blue; */
-  background-color: #9e6c31;
+
+  /* background-color: #9e6c31; */
+  background-color: #303134;
+
   height: 1vh;
 `;
