231c9b5a531fd4d4191ce28ce57c80e123a004a4
Dotoli <zzphoodev@gmail.com>
2022-10-12 18:32:08 +0900

    Feat: 게시글

--- src/components/Community/BodyCommunity.jsx
+++ src/components/Community/BodyCommunity.jsx
@@ -2,51 +2,40 @@ import React from "react";
 import styled from "styled-components";
 import { ContentCommunity } from "./index";
 
-import * as S from "./styeld";
-
 const BodyCommunity = ({ title, author, stack }) => {
   return (
     <>
-      <S.Body>
+      <BodyStyled>
         <ContentCommunity
           title={`이거모에여`}
           author={"코린이"}
           stack={"frontend"}
         />
-        <ContentCommunity
-          title={`제 코드 좀 봐주세요`}
-          author={"응애개발자"}
-          stack={"backend"}
-        />
-        <ContentCommunity
-          title={`도대체 이게 뭐죠`}
-          author={"이거모에용"}
-          stack={"backend"}
-        />
-        <ContentCommunity
-          title={`React 이거 맞아요? 진짜요?`}
-          author={"개발자(진)"}
-          stack={"frontend"}
-        />
-        <ContentCommunity
-          title={`서버 배포하는데 이거 맞습니까`}
-          author={"백조아"}
-          stack={"backend"}
-        />
         <ContentCommunity />
-        <ContentCommunity />
-        <ContentCommunity />
-        <ContentCommunity />
-        <ContentCommunity />
-        <ContentCommunity />
-        <ContentCommunity />
-        <ContentCommunity />
-        <ContentCommunity />
-
         <div> 1 2 3 4 5 </div>
-      </S.Body>
+      </BodyStyled>
     </>
   );
 };
 
 export default BodyCommunity;
+
+const BodyStyled = styled.div`
+  /* border: 1px dashed black; */
+
+  border-radius: 50px;
+  background-color: black;
+  opacity: 0.95;
+  transition: 0.5s;
+
+  color: white;
+  font-size: 2.5rem;
+  font-family: "neodgm";
+
+  margin: auto;
+  width: 70vw;
+  height: 70vh;
+  padding-top: 1vh;
+
+  text-align: center;
+`;
